64f8380992abeb75d82a922c43df817d70aacc83
HerveDelaunay <delaunay.herve06@gmail.com>
2022-04-29 10:35:23 +0200

    tailwind setup: done

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,14 @@
-import './styles.css';
-import React from 'react';
-import InputField from '../InputField/InputField';
+import './styles.css'
+import React from 'react'
+import InputField from '../InputField/InputField'
 
 const App: React.FC = () => {
 return(
     <div className='App'>
-        <span className="heading">Meteor</span>
+        <span className="heading ml-4">Meteor</span>
         <InputField/>
     </div>
-);
+)
 }
 
-export default App;
+export default App
--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const InputField: React.FC = () => {
   return (
-    <div>InputField</div>
+    <div>
+      <form action="submit">
+        <input type="text" placeholder='rechercher'/>
+        <button type='submit'>Chercher une ville</button>
+      </form>
+    </div>
   )
 }
 
