32393dfae0b2240ab7e851570176662a393272a6
HerveDelaunay <delaunay.herve06@gmail.com>
2022-05-03 17:08:57 +0200

    feat-apiCall: get weather wip

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,19 +7,29 @@ const App: React.FC = () => {
 	const [city, setCity] = useState<string>("");
 	const [lat, setLat] = useState<number>();
 	const [lon, setLon] = useState<number>();
-	const apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q='
+	const apiUrl = 'http://api.openweathermap.org'
 	const apiKey= '8e808a4b2170a7bdd2be0675e5e1200e'
 	const handleFormSubmit = (event:React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault()
 		axios({
 			method: 'get',
-			url: `${apiUrl}${city}&limit=1&appid=${apiKey}`
+			url: `${apiUrl}/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
 		})
 			.then(async(res) => {
 				setLat(res.data[0].lat)
 				setLon(res.data[0].lon)
 			})
 			.catch((err) => console.log(err))
+		if (lat && lon) {
+			axios({
+				method: 'get',
+				url: `${apiUrl}/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+			})
+			.then(async(res) => {
+				console.log(res.data)
+			})
+			.catch((err) => console.log(err))
+		}
 	}
 
 	return(
