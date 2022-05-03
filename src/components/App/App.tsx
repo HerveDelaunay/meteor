11ddd5c7aa274498c1accc66bc579b4118348244
HerveDelaunay <delaunay.herve06@gmail.com>
2022-05-03 16:39:40 +0200

    feat-apiCall: get latitude&longitude done

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,16 +5,19 @@ import axios from 'axios'
 
 const App: React.FC = () => {
 	const [city, setCity] = useState<string>("");
+	const [lat, setLat] = useState<number>();
+	const [lon, setLon] = useState<number>();
 	const apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q='
 	const apiKey= '8e808a4b2170a7bdd2be0675e5e1200e'
 	const handleFormSubmit = (event:React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault()
 		axios({
 			method: 'get',
-			url: `${apiUrl}${city}&limit=5&appid=${apiKey}`
+			url: `${apiUrl}${city}&limit=1&appid=${apiKey}`
 		})
 			.then(async(res) => {
-				console.log(res.data);
+				setLat(res.data[0].lat)
+				setLon(res.data[0].lon)
 			})
 			.catch((err) => console.log(err))
 	}
