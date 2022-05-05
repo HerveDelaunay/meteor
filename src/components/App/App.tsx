ce08bc4d3859d76211e1805484e1953548a70789
HerveDelaunay <delaunay.herve06@gmail.com>
2022-05-05 15:19:57 +0200

    feat-apiCall: fetch weather data done

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,35 +3,38 @@ import React, { useState } from 'react'
 import InputField from '../InputField/InputField'
 import axios from 'axios'
 
+
 const App: React.FC = () => {
 	const [city, setCity] = useState<string>("");
 	const [lat, setLat] = useState<number>();
 	const [lon, setLon] = useState<number>();
 	const apiUrl = 'http://api.openweathermap.org'
-	const apiKey= '8e808a4b2170a7bdd2be0675e5e1200e'
+	const apiKey= process.env.REACT_APP_API_KEY
 	const handleFormSubmit = (event:React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault()
-		axios({
-			method: 'get',
-			url: `${apiUrl}/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
-		})
-			.then(async(res) => {
-				setLat(res.data[0].lat)
-				setLon(res.data[0].lon)
+		getWeatherData()
+	}
+	const getWeatherData = async () => {
+		try {
+			const getCoordinates = await axios({
+				method: 'get',
+				url: `${apiUrl}/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
 			})
-			.catch((err) => console.log(err))
-		if (lat && lon) {
-			axios({
+			const lat = getCoordinates.data[0].lat
+			const lon = getCoordinates.data[0].lon
+			setLat(lat)
+			setLon(lon)
+			const getWeather = await axios({
 				method: 'get',
 				url: `${apiUrl}/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
 			})
-			.then(async(res) => {
-				console.log(res.data)
-			})
-			.catch((err) => console.log(err))
+			console.log(getWeather)
+		}
+		catch(error){
+			console.log(error);
 		}
 	}
-
+	
 	return(
 		<div className='min-h-screen flex flex-col bg-gradient-to-r from-blue-200 via-purple-200 to-pink-100'>
 			<h1 className="flex justify-center mt-20 text-4xl text-gray-600 font-roboto overline">METEOR</h1>
