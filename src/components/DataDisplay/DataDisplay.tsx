0315f9570232e0566f3700d0d01482d490f35f8d
HerveDelaunay <delaunay.herve06@gmail.com>
2022-05-17 17:05:07 +0200

    feat-dataDisplay: fix card style + converting functions

--- src/components/DataDisplay/DataDisplay.tsx
+++ src/components/DataDisplay/DataDisplay.tsx
@@ -7,14 +7,11 @@ interface Props {
 }
 
 const DataDisplay: React.FC<Props> = ({data, city}) => {
-  const kelvinToCelsius = (x:number | undefined) => {
-    if (x !== undefined) {
-      const celsius = Math.round(x-273.15)
-      return celsius
-    }
+  const kelvinToCelsius = (x: number | undefined) => {
+    if (x !== undefined) return Math.round(x-273.15)
  }
   const hectopascalToBar = (x: number | undefined) => {
-    if (x !== undefined) return x/1000
+    if (x !== undefined) return Math.round(x/1000)
   }
  
   return (
@@ -23,28 +20,28 @@ const DataDisplay: React.FC<Props> = ({data, city}) => {
       <h1 className='font-roboto overline uppercase text-4xl text-gray-600 basis-full text-center mt-20'>{city}</h1>
       <div className="h-2/3 mt-10 flex place-content-evenly flex-row flex-wrap items-center">
         <div className='flex flex-col items-center justify-center text-gray-600 font-roboto text-xl bg-blue-200 p-3 drop-shadow-2xl rounded-md transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 w-48 h-64'>
-          <div className='text-8xl mt-8 relative'>{kelvinToCelsius(data?.main.temp)}<span className='text-base absolute top-0 right-0'>°C</span></div>
-          <div className='border-t border-black mt-16 p-1'>Temperature</div>
+          <div className='text-8xl mt-12 p-1 relative'>{kelvinToCelsius(data?.main.temp)}<span className='text-base absolute top-0 right-0'>°C</span></div>
+          <div className='border-t border-black mt-12 p-1'>Temperature</div>
         </div>
         <div className='flex flex-col items-center justify-center text-gray-600 font-roboto text-xl bg-gradient-to-r from-blue-200 to-purple-200 p-3 drop-shadow-2xl rounded-md transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 w-48 h-64'>
-          <div className='text-8xl mt-8 relative'>{kelvinToCelsius(data?.main.feels_like)}<span className='text-base absolute top-0 right-0'>°C</span></div>
-          <div className='border-t border-black mt-16 p-1'>Ressenti</div>
+          <div className='text-8xl mt-12 p-1 relative'>{kelvinToCelsius(data?.main.feels_like)}<span className='text-base absolute top-0 right-0'>°C</span></div>
+          <div className='border-t border-black mt-12 p-1'>Ressenti</div>
         </div>
         <div className='flex flex-col items-center justify-center text-gray-600 font-roboto text-xl bg-purple-200 p-3 drop-shadow-2xl rounded-md transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 w-48 h-64'>
-          <div className='text-8xl mt-8 relative'>{kelvinToCelsius(data?.main.temp_min)}<span className='text-base absolute top-0 right-0'>°C</span></div>
-          <div className='border-t border-black mt-16 p-1'>Temperature min</div>
+          <div className='text-8xl mt-12 p-2 relative'>{kelvinToCelsius(data?.main.temp_min)}<span className='text-base absolute top-0 right-0'>°C</span></div>
+          <div className='border-t border-black mt-12 p-1'>Temperature min</div>
         </div>
         <div className='flex flex-col items-center justify-center text-gray-600 font-roboto text-xl bg-purple-200 p-3 drop-shadow-2xl rounded-md transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 w-48 h-64'>
-          <div className='text-8xl mt-8 relative'>{kelvinToCelsius(data?.main.temp_max)}<span className='text-base absolute top-0 right-0'>°C</span></div>
-          <div className='border-t border-black mt-16 p-1'>Temperature max</div>
+          <div className='text-8xl mt-12 p-1 relative'>{kelvinToCelsius(data?.main.temp_max)}<span className='text-base absolute top-0 right-0'>°C</span></div>
+          <div className='border-t border-black mt-12 p-1'>Temperature max</div>
         </div>
         <div className='flex flex-col items-center justify-center text-gray-600 font-roboto text-xl bg-gradient-to-r from-purple-200 to-pink-100 p-3 drop-shadow-2xl rounded-md transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 w-48 h-64'>
-          <div className='text-8xl mt-8 relative'>{hectopascalToBar(data?.main.pressure)}<span className='text-base absolute top-0 right-0'>bar</span></div>
-          <div className='border-t border-black mt-16 p-1'>Pression</div>
+          <div className='text-8xl mt-12 p-2 relative'>{hectopascalToBar(data?.main.pressure)}<span className='text-base absolute top-0 right-0'>bar</span></div>
+          <div className='border-t border-black mt-12 p-1'>Pression</div>
         </div>
         <div className='flex flex-col items-center justify-center text-gray-600 font-roboto text-xl bg-pink-100 p-3 drop-shadow-2xl rounded-md transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 w-48 h-64'>
-          <div className='text-8xl mt-8 relative'>{data?.main.humidity}<span className='text-base absolute top-0 right-0'>%</span></div>
-          <div className='border-t border-black mt-16 p-1'>Humidité</div>
+          <div className='text-8xl mt-12 p-2 relative'>{data?.main.humidity}<span className='text-base absolute top-0 right-0'>%</span></div>
+          <div className='border-t border-black mt-12 p-1'>Humidité</div>
         </div>
       </div>
     </div>
@@ -52,6 +49,3 @@ const DataDisplay: React.FC<Props> = ({data, city}) => {
 }
 
 export default DataDisplay
-
-// bg-gradient-to-r from-blueG to-yellowG
-// {kelvinToCelsius(data?.main.temp)}
